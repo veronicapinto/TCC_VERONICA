6b1e8f46893c3de8f1704c53e1baccfacabc2038
veronica <veronica@192.168.230.159>
2013-08-15 10:13:41 -0300

    Criadas as entidades Funcionario e TipoFuncionario

---
+++ GestaoPatrimonial/src/java/br/edu/ifnmg/GestaoPatrimonial/DomainModel/Funcionario.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.GestaoPatrimonial.DomainModel;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author veronica
+ */
+@Entity
+public class Funcionario implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @OneToMany
+    private TipoFuncionario tipo;
+    
+    private String nome;
+    private String login;
+    private String senha;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public TipoFuncionario getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoFuncionario tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Funcionario)) {
+            return false;
+        }
+        Funcionario other = (Funcionario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.edu.ifnmg.GestaoPatrimonial.DomainModel.Funcionario[ id=" + id + " ]";
+    }
+    
+}
