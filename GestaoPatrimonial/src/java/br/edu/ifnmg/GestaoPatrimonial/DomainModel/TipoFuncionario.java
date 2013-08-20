4ecff690a9e480d4cf385d9581d322afcb47a297
veronica <veronica@192.168.230.159>
2013-08-19 22:59:48 -0300

    Criadas as entidades Funcionario e TipoFuncionario

---
+++ GestaoPatrimonial/src/java/br/edu/ifnmg/GestaoPatrimonial/DomainModel/TipoFuncionario.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.GestaoPatrimonial.DomainModel;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author veronica
+ */
+@Entity
+public class TipoFuncionario implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof TipoFuncionario)) {
+            return false;
+        }
+        TipoFuncionario other = (TipoFuncionario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "br.edu.ifnmg.GestaoPatrimonial.DomainModel.TipoFuncionario[ id=" + id + " ]";
+    }
+    
+}
