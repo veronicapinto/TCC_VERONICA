cc58c95266b6935c680ef48f6da71708b28c884b
veronica <veronica@192.168.230.159>
2013-08-20 23:18:13 -0300

    Modificado a classe Fornecedor

--- GestaoPatrimonial/src/java/br/edu/ifnmg/GestaoPatrimonial/DomainModel/Fornecedor.java
+++ GestaoPatrimonial/src/java/br/edu/ifnmg/GestaoPatrimonial/DomainModel/Fornecedor.java
@@ -37,6 +37,7 @@ public class Fornecedor implements Serializable {
     
     private String email; 
     
+    
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date data;
            
@@ -48,6 +49,73 @@ public class Fornecedor implements Serializable {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+    
+    
+    
 
     
     @Override
