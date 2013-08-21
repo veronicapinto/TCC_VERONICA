1e3761dd7d74dbc56865039c33ff84868f749820
veronica <veronica@192.168.230.159>
2013-08-20 23:13:53 -0300

    Criadas as Entidades Conta Patrimonial e Fornecedor

--- GestaoPatrimonial/src/java/br/edu/ifnmg/GestaoPatrimonial/DomainModel/Fornecedor.java
+++ GestaoPatrimonial/src/java/br/edu/ifnmg/GestaoPatrimonial/DomainModel/Fornecedor.java
@@ -26,8 +26,6 @@ public class Fornecedor implements Serializable {
     private String nome;
     
     private String cnpj;
-    
-    private char  Sexo;
      
     private String telefone;
     
@@ -49,78 +47,6 @@ public class Fornecedor implements Serializable {
     
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public char getSexo() {
-        return Sexo;
-    }
-
-    public void setSexo(char Sexo) {
-        this.Sexo = Sexo;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getData() {
-        return data;
-    }
-
-    public void setData(Date data) {
-        this.data = data;
     }
 
     
