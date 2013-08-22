e23e977370648337272162a5837b361243b61088
veronica <veronica@192.168.230.159>
2013-08-22 10:29:06 -0300

    alterei o relacionamento entre Funcionario e TipoFuncionario

--- GestaoPatrimonial/src/java/br/edu/ifnmg/GestaoPatrimonial/DomainModel/Funcionario.java
+++ GestaoPatrimonial/src/java/br/edu/ifnmg/GestaoPatrimonial/DomainModel/Funcionario.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 /**
@@ -22,7 +23,7 @@ public class Funcionario implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
-    @OneToMany
+    @ManyToOne
     private TipoFuncionario tipo;
     
     private String nome;
