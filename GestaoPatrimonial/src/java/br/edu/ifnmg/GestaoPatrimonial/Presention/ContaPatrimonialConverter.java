f72c67d9141d7940e5a8e55d713022a79ce09cdc
veronica <veronica@192.168.230.159>
2013-10-01 11:45:56 -0300

    Criada a contaPatrimonialconverter

---
+++ GestaoPatrimonial/src/java/br/edu/ifnmg/GestaoPatrimonial/Presention/ContaPatrimonialConverter.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.GestaoPatrimonial.Presention;
+
+import br.edu.ifnmg.GestaoPatrimonial.DomainModel.ContaPatrimonial;
+import br.edu.ifnmg.GestaoPatrimonial.DomainModel.IContaPatrimonialRepositorio;
+import br.edu.ifnmg.GestaoPatrimonial.DomainModel.IRepositorio;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+/**
+ *
+ * @author veronica
+ */
+@Named(value = "contaPatrimonialConverter")
+@SessionScoped
+public class ContaPatrimonialConverter implements Serializable, Converter {
+
+    @EJB
+    IContaPatrimonialRepositorio daoContapatrimonial;
+
+    /**
+     * Creates a new instance of ContaPatrimonialConverter
+     */
+    public ContaPatrimonialConverter() {
+    }
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        if (value == null || value.trim().equals("")) {
+            return null;
+
+        } else {
+            Long id = Long.parseLong(value);
+            return daoContapatrimonial.Abrir(id);
+
+        }
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+
+        if (value == null || value.toString().equals("")) {
+            return "";
+        } else {
+            ContaPatrimonial cp = (ContaPatrimonial) value;
+            return cp.getId().toString();
+        }
+    }
+}
